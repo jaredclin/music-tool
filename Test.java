af0296b1809ba2bb7e70f298884cedfd2e6bd1b1
jaredclin <jaredc0307@gmail.com>
2020-07-17 11:17:59 -0400

    refactor key conversion
    missing text

--- Test.java
+++ Test.java
@@ -3,7 +3,7 @@ public class Test {
         Key c = new Key("C", false);
         System.out.println("C"+":\t" + c.toString());
         Key cm = new Key("C", true);
-        System.out.println("C"+":\t" + cm.toString());
+        System.out.println("Cm"+":\t" + cm.toString());
         Key csharp = new Key("C#", false);
         System.out.println("C#"+":\t" + csharp.toString());
 
